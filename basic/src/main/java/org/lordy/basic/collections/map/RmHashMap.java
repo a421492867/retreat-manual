d81dd28f15c9e016305fea9c3f9fb5966a67a947
a421492867 <421492867@qq.com>
2021-12-02 20:15:16 +0800

    nothing

---
+++ basic/src/main/java/org/lordy/basic/collections/map/RmHashMap.java
@@ -0,0 +1,10 @@
+package org.lordy.basic.collections.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RmHashMap {
+
+
+    Map<String, Object> map = new HashMap<String, Object>();
+}
