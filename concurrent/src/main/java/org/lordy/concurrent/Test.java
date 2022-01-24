abe26641d1d2a584bc871856c31a43b08de81174
unknown <yueshuaipeng@cecbrain.com>
2022-01-24 14:23:17 +0800

    .

--- concurrent/src/main/java/org/lordy/concurrent/Test.java
+++ concurrent/src/main/java/org/lordy/concurrent/Test.java
@@ -1,12 +1,17 @@
 package org.lordy.concurrent;
 
+import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class Test {
 
     static ReentrantLock reentrantLock = new ReentrantLock();
 
-    public static void main(String[] args) {
+    ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+
+    public static void main(String[] args) throws Exception{
         reentrantLock.lock();
+        reentrantLock.lockInterruptibly();
     }
 }
